afc3c4e491c3f927e62d179d320e8e4f905d6ac3
ankurshashwat <1nkurshashwat@gmail.com>
2025-01-10 19:24:50 +0530

    Implement workflow management components

---
+++ components/workflows/WorkflowCard.tsx
@@ -0,0 +1,129 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Workflow } from "@prisma/client";
+import {
+  MoreVerticalIcon,
+  PlayIcon,
+  ShuffleIcon,
+  SquarePen,
+  TrashIcon,
+} from "lucide-react";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import { WorkflowStatus } from "@/types/workflow";
+import TooltipWrapper from "../shared/TooltipWrapper";
+import DuplicateWorkflowDialog from "./DuplicateWorkflowDialog";
+import DeleteWorkflowDialog from "./DeleteWorkflowDialog";
+
+export default function WorkflowCard({ workflow }: { workflow: Workflow }) {
+  const isDraft = workflow.status === WorkflowStatus.DRAFT;
+
+  return (
+    <Card className="dark:bg-[#121212] border border-separate shadow-sm rounded-lg overflow-hidden hover:shadow-md group/card">
+      <CardContent className="p-4 flex items-center justify-between h-[100px]">
+        <div className="flex items-center just space-x-3">
+          <div>
+            {isDraft ? (
+              <SquarePen className="h-5 w-5 text-muted-foreground" />
+            ) : (
+              <PlayIcon className="h-5 w-5 text-muted-foreground" />
+            )}
+          </div>
+          <div>
+            <h3 className="text-base font-bold flex items-center">
+              <TooltipWrapper content={workflow.description}>
+                <Link
+                  href={`/workflow/editor/${workflow.id}`}
+                  className="flex items-center hover:underline"
+                >
+                  {workflow.name}
+                </Link>
+              </TooltipWrapper>
+              {isDraft && (
+                <span className="ml-2 px-2 py-0.5 text-xs font-semibold bg-[#F0F4F9] text-[#000000] rounded-full">
+                  Draft
+                </span>
+              )}
+              <DuplicateWorkflowDialog workflowId={workflow.id} />
+            </h3>
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <Link
+            href={`/workflow/editor/${workflow.id}`}
+            className={cn(
+              buttonVariants({
+                variant: "outline",
+                size: "sm",
+              }),
+              "flex items-center gap-2"
+            )}
+          >
+            <ShuffleIcon size={16} />
+            Edit
+          </Link>
+          <WorkflowActions
+            workflowName={workflow.name}
+            workflowId={workflow.id}
+          />
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+function WorkflowActions({
+  workflowName,
+  workflowId,
+}: {
+  workflowName: string;
+  workflowId: string;
+}) {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+  return (
+    <>
+      <DeleteWorkflowDialog
+        open={showDeleteDialog}
+        setOpen={setShowDeleteDialog}
+        workflowName={workflowName}
+        workflowId={workflowId}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="sm">
+            <TooltipWrapper content="More actions">
+              <div className="flex items-center justify-center w-full h-full">
+                <MoreVerticalIcon size={18} />
+              </div>
+            </TooltipWrapper>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="text-destructive flex items-center gap-2"
+            onSelect={() => {
+              setShowDeleteDialog((prev) => !prev);
+            }}
+          >
+            <TrashIcon size={16} />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
