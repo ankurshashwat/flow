5120ce5e218e9402ee69789564cda926d22e3ecd
ankurshashwat <1nkurshashwat@gmail.com>
2025-01-10 19:18:37 +0530

    Implement server-side actions for workflow management

---
+++ lib/actions/workflows/workflows.action.ts
@@ -0,0 +1,122 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import {
+  createWorkflowSchema,
+  createWorkflowSchemaType,
+  duplicateWorkflowSchema,
+  duplicateWorkflowSchemaType,
+} from "@/lib/validations";
+import { WorkflowStatus } from "@/types/workflow";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export async function getUserWorkflows() {
+  const { userId } = auth();
+  if (!userId) {
+    throw new Error("Unauthenticated");
+  }
+
+  return prisma.workflow.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+}
+
+export async function createWorkflow(form: createWorkflowSchemaType) {
+  const { success, data } = createWorkflowSchema.safeParse(form);
+
+  if (!success) {
+    throw new Error("Invalid form data");
+  }
+
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error("Unauthenticated");
+  }
+
+  // const initialFlow: { nodes: AppNode[]; edges: Edge[] } = {
+  //   nodes: [],
+  //   edges: [],
+  // };
+
+  //add the flow entry point
+  // initialFlow.nodes.push(createFlowNode(TaskType.LAUNCH_BROWSER));
+
+  const result = await prisma.workflow.create({
+    data: {
+      userId,
+      status: WorkflowStatus.DRAFT,
+      definition: "TODO",
+      // definition: JSON.stringify(initialFlow),
+      ...data,
+    },
+  });
+
+  if (!result) {
+    throw new Error("Failed to create workflow");
+  }
+
+  redirect(`/workflow/editor/${result.id}`);
+}
+
+export async function duplicateWorkflow(form: duplicateWorkflowSchemaType) {
+  const { success, data } = duplicateWorkflowSchema.safeParse(form);
+
+  if (!success) {
+    throw new Error("Invalid form data");
+  }
+
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error("Unauthenticated");
+  }
+
+  const sourceWorkflow = await prisma.workflow.findUnique({
+    where: { id: data.workflowId, userId },
+  });
+
+  if (!sourceWorkflow) {
+    throw new Error("workflow not found");
+  }
+
+  const result = await prisma.workflow.create({
+    data: {
+      userId,
+      name: data.name,
+      description: data.description,
+      status: WorkflowStatus.DRAFT,
+      definition: sourceWorkflow.definition,
+    },
+  });
+
+  if (!result) {
+    throw new Error("Failed to duplicate workflow");
+  }
+
+  revalidatePath("/workflows");
+}
+
+export async function deleteWorkflow(id: string) {
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error('Unauthenticated');
+  }
+
+  await prisma.workflow.delete({
+    where: {
+      id,
+      userId,
+    },
+  });
+
+  revalidatePath('/workflows');
+}
